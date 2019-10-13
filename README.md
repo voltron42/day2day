fdee0931f73a104965c10a4947f2907f37f305ec
Daniel Johnson <dajohnson1978@gmail.com>
2019-10-13 13:36:39 -0500

    fixing issues

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Poc
+# day2day
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.1.
 
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/poc'),
+      dir: require('path').join(__dirname, '../coverage/day2day'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
